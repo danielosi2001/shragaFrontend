2e0cc9268575db612e977950fcc51368337f3a69
almog1020 <tshukati4@gmail.com>
2023-10-09 14:13:39 +0300

    Add componest program detail

--- src/components/layout/programs/programDetails/programsDetails.tsx
+++ src/components/layout/programs/programDetails/programsDetails.tsx
@@ -1,7 +1,17 @@
-export default function ProgramsDetails(): JSX.Element {
+
+interface system {
+    name: string
+    src: string
+    link: string
+    description: string
+}
+export default function programsDetails(props:system): JSX.Element {
     return (
         <div>
-ADS
+            <h2>{props.name}</h2>
+            <img src={props.src}/>
+            <a href={props.link} ></a>
+            <p>{props.description}</p>
         </div>
     )
 }
--- src/components/layout/programs/programs.tsx
+++ src/components/layout/programs/programs.tsx
@@ -1,7 +1,36 @@
+
+import ProgramsDetails from "./programDetails/ProgramsDetails";
+
+const details = [
+    {
+        name: "git",
+        imgeUrl: "https://git-scm.com/images/logo@2x.png",
+        link: "https://git-scm.com/",
+        desc: "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency"
+    },
+    {
+        name: "Jira",
+        imgeUrl: "https://www.atlassian.com/software/jira",
+        link: "https://www.atlassian.com/software/jira",
+        desc: "Jira is a proprietary issue tracking product developed by Atlassian that allows bug tracking and agile project management."
+    }
+];
+
 export default function Programs(): JSX.Element {
     return (
         <div>
-            ASD
+            <ProgramsDetails
+                name={details[0].name}
+                src={details[0].imgeUrl}
+                link={details[0].link}
+                description={details[0].desc}
+            />
+            <ProgramsDetails
+                name={details[1].name}
+                src={details[1].imgeUrl}
+                link={details[1].link}
+                description={details[1].desc}
+            />
         </div>
-    )
+    );
 }
